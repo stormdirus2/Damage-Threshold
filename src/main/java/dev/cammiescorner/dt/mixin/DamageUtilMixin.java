59f934959dd7eaa319f0ac12215cdb64c432ce3f
CammiePone <inazuma261@gmail.com>
2021-01-25 22:14:04 -0500

    Forgot to update the repo

--- src/main/java/dev/cammiescorner/dt/mixin/DamageUtilMixin.java
+++ src/main/java/dev/cammiescorner/dt/mixin/DamageUtilMixin.java
@@ -1,18 +1,19 @@
 package dev.cammiescorner.dt.mixin;
 
 import net.minecraft.entity.DamageUtil;
+import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(DamageUtil.class)
 public class DamageUtilMixin
 {
-	/**
-	 * @author Cammie
-	 */
-	@Overwrite
-	public static float getDamageLeft(float damage, float armor, float armorToughness)
+	@Inject(method = "getDamageLeft", at = @At("HEAD"), cancellable = true)
+	private static void getDamageLeft(float damage, float armor, float armorToughness, CallbackInfoReturnable<Float> info)
 	{
-		return Math.max(1, damage - (armor / (2 - Math.min(1, armorToughness / 20))));
+		info.setReturnValue(Math.max(1, damage - (armor / (4 - Math.min(1, armorToughness / 10)))));
 	}
 }
